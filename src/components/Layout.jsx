3ebf35d1f212f0a3ecd15ecf2d37167ad9a3627b
afifym <mohamed.afify323@gmail.com>
2022-03-23 16:28:25 +0200

    Add og meta tags, update dependancies

--- src/components/Layout.jsx
+++ src/components/Layout.jsx
@@ -6,11 +6,14 @@ const Layout = ({ children }) => {
     <>
       <Helmet>
         <html lang="en" />
-        <title>Mohamed Afify Frontend Developer</title>
+        <title>Mohamed, A Frontend Developer</title>
         <meta
           name="description"
           content="A personal website and portfolio for a frontend developer skilled in both development and designing aspects"
         />
+        <meta property="og:title" content="Mohamed, A Frontend Developer" />
+        <meta property="og:description" content="Get from SEO newbie to SEO pro in 8 simple steps." />
+        <meta property="og:image" content="/og.jpg" />
       </Helmet>
       <div className="layout">
         <div className="content">{children}</div>
