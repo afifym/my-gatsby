bc884b5f7848f30224b1f9a1b01f835944523709
afifym <mohamed.afify323@gmail.com>
2022-03-23 16:29:26 +0200

    Fix og description

--- src/components/Layout.jsx
+++ src/components/Layout.jsx
@@ -9,10 +9,10 @@ const Layout = ({ children }) => {
         <title>Mohamed, A Frontend Developer</title>
         <meta
           name="description"
-          content="A personal website and portfolio for a frontend developer skilled in both development and designing aspects"
+          content="A frontend developer skilled in both development and designing aspects"
         />
         <meta property="og:title" content="Mohamed, A Frontend Developer" />
-        <meta property="og:description" content="Get from SEO newbie to SEO pro in 8 simple steps." />
+        <meta property="og:description" content="A frontend developer skilled in both development and designing aspects" />
         <meta property="og:image" content="/og.jpg" />
       </Helmet>
       <div className="layout">
